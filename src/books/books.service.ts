2350787bbeda45400b376206102e41ad6d27010b
minhtriet123 <tranvuminhtriet@gmail.com>
2021-07-21 11:00:02 +0700

    ver 1.5

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -51,13 +51,12 @@ export class BooksService {
       where: { id, is_deleted: false },
       relations: ['author', 'category'],
     });
-    // if (!book) throw new NotFoundException(`No Book with ID: ${id} is found`);
-    // if (title) book.title = title;
-    // if (authorId) book.author.id = authorId;
-    // if (categoryId) book.category.id = categoryId;
-    // if (price) book.price = price;
-    // if (description) book.description = description;
-    const updatedBook = { ...book, ...updateBookDto };
-    return await this.booksRepository.save(updatedBook);
+    if (!book) throw new NotFoundException(`No Book with ID: ${id} is found`);
+    if (title) book.title = title;
+    if (authorId) book.author.id = authorId;
+    if (categoryId) book.category.id = categoryId;
+    if (price) book.price = price;
+    if (description) book.description = description;
+    return await this.booksRepository.save(book);
   }
 }
