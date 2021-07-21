1bd89bdc479cb0cd8b0f55b5e61f4b922e968e9a
minhtriet123 <tranvuminhtriet@gmail.com>
2021-07-21 12:17:17 +0700

    add file to deploy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe()); // use validation
-  const port = 5000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
